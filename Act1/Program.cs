26ba21904987a3e0832bdde2357b87250ce5c2c7
panjialbir <panjialbir10@gmail.com>
2022-10-06 18:45:22 +0700

    1

--- Act1/Program.cs
+++ Act1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    { //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        //fungsi atau method untuk menerima masukan
+        public void read()
+            {
+            //menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+                Console.WriteLine("");
+                Console.WriteLine("========================================");
+                Console.WriteLine("Masukkan Elemen Array");
+                Console.WriteLine("========================================");
+
+                //pengguna memasukkan elemen pada array
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("<" + (i + 1) + ">");
+                    String s1 = Console.ReadLine();
+                    a[i] = Int32.Parse(s1);
+                }
+            }
+            public void display()
+            {
+                //menampilkan array yang tersusun
+                Console.WriteLine("");
+                Console.WriteLine("================================");
+                Console.WriteLine("Elemen Array Yang Telah Tersusun");
+                Console.WriteLine("================================");
+
+            }
+        }
+    }
+}
